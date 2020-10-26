89cd8435b28bd540cc1a8662ab112eaf6eca9425
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2020-10-26 21:13:34 +0900

    間違い探し画像領域の推定アルゴリズムを改良してエラー解消
    - 白黒反転させて領域検出し、一番面積の大きな領域を間違い探し画像領域と推定することで、エラー解消
    
    - まだ未検出や誤検出はいくつか残っているが、最低限のレベルには到達したという認識

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 from PhotoHunt import PhotoHunt
 
+# Todo: 各URLあたり1～2個の未検出、誤検出等の課題はあるが、概ね意図通り動作する状態となった
 url_list = [
-    # Todo: 1個未発見
     "https://www.saizeriya.co.jp/entertainment/images/1710/body.png",
-    # Perfect!
     "https://www.saizeriya.co.jp/entertainment/images/1801/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗してエラー
-    # "https://www.saizeriya.co.jp/entertainment/images/1804/body.png",
-    # Todo: 2個未発見
+    "https://www.saizeriya.co.jp/entertainment/images/1804/body.png",
     "https://www.saizeriya.co.jp/entertainment/images/1806/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗しておりエラーにはなっていないが精度低すぎる
     "https://www.saizeriya.co.jp/entertainment/images/1810/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗しておりエラーにはなっていないが精度低すぎる
     "https://www.saizeriya.co.jp/entertainment/images/1812/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗しておりエラーにはなっていないが精度低すぎる
     "https://www.saizeriya.co.jp/entertainment/images/1904/body.png",
-    # Perfect!
     "https://www.saizeriya.co.jp/entertainment/images/1907/body.png",
-    # Perfect!
     "https://www.saizeriya.co.jp/entertainment/images/1910/body.png",
-    # Todo: 1個間違いでない箇所を間違いと誤検知
     "https://www.saizeriya.co.jp/entertainment/images/1912/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗しておりエラーにはなっていないが精度低すぎる
     "https://www.saizeriya.co.jp/entertainment/images/2003/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗しておりエラーにはなっていないが精度低すぎる
     "https://www.saizeriya.co.jp/entertainment/images/2007/body.png",
-    # Todo: 間違い探し画像の領域推定に失敗してエラー
     "https://www.saizeriya.co.jp/entertainment/images/2009/body.png"
 ]
 
